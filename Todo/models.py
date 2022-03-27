cd2ac940be4480d53e1c4164bb86a0e60ee9d46b
Muhammad Abdullah Khan <abdullahkhan.smiu@gmail.com>
2022-03-27 09:51:16 +0500

    Commit

--- Todo/models.py
+++ Todo/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Todo(models.Model):
-    title = models.CharField(max_length = 10)
-    image = models.CharField(max_length = 200)
-    rent = models.CharField(max_length = 10)
+    title = models.CharField(max_length = 10,null=True)
+    image = models.CharField(max_length = 200,null=True)
+    rent = models.CharField(max_length = 10,null=True)
      
 
     def __str___(self):
